d6dc6d2efc8a4b9c860abcbddd1a24e7273bc4e2
Gerardo Macedo <gerardomacedo22@gmail.com>
2024-09-24 11:31:28 -0700

    Agregacion de graficas

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import plotly.express as px
 
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+st.header("Catalgo de Carros USA")
+print()
 hist_button = st.button('Construir histograma')  # crear un botón
+
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
@@ -18,8 +22,23 @@ if hist_button:  # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 
+scatter_button = st.button(
+    'Construir un diagrama de dispersión')  # Crear botón
+if scatter_button:
+    # Escribir mensaje
+    st.write('Creación de un diagrama de dispersión')
+    # Creacion de diagrama de dispersion
+    fig1 = px.scatter(car_data, x="model_year", y="price", color="type")
+    # monstrar el diagrama
+    st.plotly_chart(fig1, use_container_width=True)
 # Ejemplo de Checkbox
+
 build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram:  # si la casilla de verificación está seleccionada
+    # Mensaje
     st.write('Construir un histograma para la columna odómetro')
+    # Creacion de Histograma
+    fig2 = px.histogram(car_data, x='days_listed')
+    # Monstrar el histograma
+    st.plotly_chart(fig2, use_container_width=True)
