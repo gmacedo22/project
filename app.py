bfdb368df77fc5292f66172e36d991286cb8288a
Gerardo Macedo <gerardomacedo22@gmail.com>
2024-09-24 10:35:15 -0700

    Update

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# Ejemplo de Checkbox
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
